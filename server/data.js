b2fcf11009174b0664546a3f99a00d8269d6ea31
Hack Reactor Students <accounts@hackreactor.com>
2017-03-14 20:21:02 -0700

    Complete dummy data for development

--- server/data.js
+++ server/data.js
@@ -1,36 +1,31 @@
 const users = [
   {
-    id: 1,
     firstname: 'Anthony',
     lastname: 'Bianco'
   },
 
    {
-    id: 2,
     firstname: 'Nick',
     lastname: 'Below'
   },
 
    {
-    id: 3,
     firstname: 'David',
     lastname: 'Brodie'
   },
    {
-    id: 4,
     firstname: 'Gus',
     lastname: 'Brennan'
   },
 
    {
-    id: 5,
     firstname: 'Fred',
     lastname: 'Zirdung'
   },
 ];
 const events = [
   {
-    id: 1,
+    owner: 1,
     title: 'Beach Party',
     description: 'Let\'s go to Ocean Beach while the sun is out! BYOB and don\'t joke about sharks, I\'ll kick you out for real.',
     location: 'Ocean Beach, SF, right off the N Judah.',
@@ -39,7 +34,7 @@ const events = [
     attendees: 10
   },
   {
-    id: 2,
+    owner: 2,
     title: 'BBQ',
     description: 'Vegan Only; don\'t be a jerk about it.',
     location: '456 Fillmore Street, San Francisco, CA 94117',
@@ -48,7 +43,7 @@ const events = [
     attendees: 3
   },
   {
-    id: 3,
+    owner: 5,
     title: 'Movie',
     description: 'Let\'s go see Moonlight',
     location: 'Downtown AMC Metreon',
@@ -56,5 +51,68 @@ const events = [
     time: '20:00:00',
     attendees: 6
   }
+];
+
+const usersEvents = [
+  {
+    event: 1,
+    user: 2,
+    'in?': pending
+  },
+  {
+    event: 1,
+    user: 3,
+    'in?': pending
+  },
+  {
+    event: 1,
+    user: 4,
+    'in?': rejected
+  },
+  {
+    event: 1,
+    user: 5,
+    'in?': pending
+  },
+  {
+    event: 2,
+    user: 1,
+    'in?': accepted
+  },
+  {
+    event: 2,
+    user: 3,
+    'in?': rejected
+  },
+  {
+    event: 2,
+    user: 4,
+    'in?': accepted
+  },
+  {
+    event: 2,
+    user: 5,
+    'in?': pending
+  },
+  {
+    event: 3,
+    user: 1,
+    'in?': rejected
+  },
+  {
+    event: 3,
+    user: 2,
+    'in?': accepted
+  },
+  {
+    event: 3,
+    user: 3,
+    'in?': pending
+  },
+  {
+    event: 3,
+    user: 4,
+    'in?': accepted
+  },
 ];
 
