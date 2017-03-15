2c0a4bf63252e8d75f6d62b389cc6072dea84f7a
Hack Reactor Students <accounts@hackreactor.com>
2017-03-15 12:22:20 -0700

    Render event titles via map

---
+++ src/client/app/EventListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EventListItem = (props) => (
+
+  <div>
+    {props.event.title}
+  </div>
+)
+
+export default EventListItem;
+
+
+  
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -4,7 +4,43 @@ import EventList from './EventList.jsx';
 
 class App extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      ownerEvents: [
+        {
+          owner: 1,
+          title: 'I Wanna Go To The Beach',
+          'short_desc': 'Beach Party',
+          description: 'Let\'s go to Ocean Beach while the sun is out! BYOB and don\'t joke about sharks, I\'ll kick you out for real.',
+          location: 'Ocean Beach, SF, right off the N Judah.',
+          date: '2017-03-14',
+          time: '05:30:00',
+          attendees: 10
+        }
+      ],
+      friendEvents: [
+        {
+          owner: 2,
+          title: 'Pig, Beer, and Bocce Ball',
+          'short_desc': 'BBQ',
+          description: 'Vegan Only; don\'t be a jerk about it.',
+          location: '456 Fillmore Street, San Francisco, CA 94117',
+          date: '2017-03-14',
+          time: '17:45:00',
+          attendees: 3
+        },
+        {
+          owner: 5,
+          title: 'Moonlight at 8',
+          'short_desc': 'Movie',
+          description: 'Let\'s go see Moonlight',
+          location: 'Downtown AMC Metreon',
+          date: '2017-03-14',
+          time: '20:00:00',
+          attendees: 6
+        }
+      ]
+    }
   }
 
   render () {
@@ -15,12 +51,11 @@ class App extends React.Component {
         <br /><br />
         Here is where owner events will go <br/>
          Owner's Events: <br/>
-        <EventList />
+        <EventList ownerEventsArr={this.state.ownerEvents}/>
 
         <br /><br />
         Here is where friend events will go <br/>
         Friend's Events: <br/>
-        <EventList />
       </div>
     )
           
