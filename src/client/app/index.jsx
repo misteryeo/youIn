46223c735462543cc20b4cab98561e3f5ac3d592
Hack Reactor Students <accounts@hackreactor.com>
2017-03-14 20:15:42 -0700

    Add app component with outline for future components

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,7 +3,19 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div>
+        <h1>You In?</h1>
+        <button>Create Event</button>
+        <br /><br />
+        Here is where owner events will go <br/>
+        Owner Event List item will go here
+        <br /><br />
+        Here is where friend events will go <br/>
+        Friend Event list item will go here
+      </div>
+    )
+          
   }
 }
 
