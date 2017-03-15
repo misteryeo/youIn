a6184831203ef667dbfbfbdb8ad1abea35e9ba73
Hack Reactor Students <accounts@hackreactor.com>
2017-03-14 20:02:38 -0700

    Initiate dummy data

---
+++ server/data.js
@@ -0,0 +1,60 @@
+const users = [
+  {
+    id: 1,
+    firstname: 'Anthony',
+    lastname: 'Bianco'
+  },
+
+   {
+    id: 2,
+    firstname: 'Nick',
+    lastname: 'Below'
+  },
+
+   {
+    id: 3,
+    firstname: 'David',
+    lastname: 'Brodie'
+  },
+   {
+    id: 4,
+    firstname: 'Gus',
+    lastname: 'Brennan'
+  },
+
+   {
+    id: 5,
+    firstname: 'Fred',
+    lastname: 'Zirdung'
+  },
+];
+const events = [
+  {
+    id: 1,
+    title: 'Beach Party',
+    description: 'Let\'s go to Ocean Beach while the sun is out! BYOB and don\'t joke about sharks, I\'ll kick you out for real.',
+    location: 'Ocean Beach, SF, right off the N Judah.',
+    date: '2017-03-14',
+    time: '05:30:00',
+    attendees: 10
+  },
+  {
+    id: 2,
+    title: 'BBQ',
+    description: 'Vegan Only; don\'t be a jerk about it.',
+    location: '456 Fillmore Street, San Francisco, CA 94117',
+    date: '2017-03-14',
+    time: '17:45:00',
+    attendees: 3
+  },
+  {
+    id: 3,
+    title: 'Movie',
+    description: 'Let\'s go see Moonlight',
+    location: 'Downtown AMC Metreon',
+    date: '2017-03-14',
+    time: '20:00:00',
+    attendees: 6
+  }
+];
+
