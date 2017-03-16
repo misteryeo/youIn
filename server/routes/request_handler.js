39a4351419e8d6e1eb47dfa2c2e0f07983cd95cc
Anthony Bianco <anthony.bianco3@gmail.com>
2017-03-15 21:55:19 -0700

    Handle POSTs to '/events' this end point creates a new event in the events table. It currently assigns a random owner. Will be updated after sign-in is enabled.

---
+++ server/routes/request_handler.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let db = require('../config');
+
+let createEvent = function(req, res) {
+  // create a random user as sub for
+  // req.session.user.user_id
+
+  let user_id = Math.ceil(Math.random() * 5);
+
+  let event = req.body;
+  event.owner = user_id;
+
+  console.log(event);
+  // query the database for events
+  db.queryAsync('INSERT into EVENTS SET ?', event)
+  .then( (result) => {
+    res.status(201).json(result);
+  })
+  .error( (err) => {
+    res.status(404).send(err, "Invalid user_id, please login");
+  });
+};
+
+module.exports.createEvent = createEvent;
