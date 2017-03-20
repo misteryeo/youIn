78714048aa0fa05f49940b8ed1a9c4d57ec17a80
David Brodie <david.brodie122@gmail.com>
2017-03-20 12:05:36 -0700

    Create FriendDetailedView stateful class component + .jsx file

---
+++ src/client/app/FriendDetailedView.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class FriendDetailedView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    //bind methods here
+
+  }
+  //insert methods here
+
+  render() {
+
+    return (
+      <div>
+        Detailed View
+      </div>
+    );
+  }
+}
+
+export default FriendDetailedView
