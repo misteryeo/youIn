f1c160f2a9ebadc618ef3f274fb737121da7e89e
David Brodie <david.brodie122@gmail.com>
2017-03-20 11:43:57 -0700

    Convert FriendEventListItem to stateful class component

--- src/client/app/FriendEventListItem.jsx
+++ src/client/app/FriendEventListItem.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-const FriendEventListItem = (props) => (
 
-  <div className="panel list-item ">
-    <span className="glyphicon glyphicon-globe col-md-1"></span> 
-    <span className="col-md-4">{props.event.title}</span>
-    <span className="col-md-4">{props.event.date} at {props.event.time}</span>
-    <span className="col-md-3">{props.event.attendees}<span> people IN</span></span>
-    <br/>
-  </div>
-)
+
+class FriendEventListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    //bind methods here
+
+  }
+  //Insert Methods Here
+
+  render() {
+
+    return (
+      <div className="panel list-item ">
+        <span className="glyphicon glyphicon-globe col-md-1"></span> 
+        <span className="col-md-4">{this.props.event.title}</span>
+        <span className="col-md-4">{this.props.event.date} at {this.props.event.time}</span>
+        <span className="col-md-3">{this.props.event.attendees}<span> people IN</span></span>
+        <br/>
+      </div>
+    );
+  }
+}
 
 export default FriendEventListItem;
 
