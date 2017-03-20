cdf9db61bfec1c78b87201c5db832929ecdd2fe6
David Brodie <david.brodie122@gmail.com>
2017-03-20 11:48:48 -0700

    Add click event, handler function, state. Detailed view not finished

--- src/client/app/FriendEventListItem.jsx
+++ src/client/app/FriendEventListItem.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-
-
 class FriendEventListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      clicked: false
     }
     //bind methods here
-
+    this.handleClickListItem = this.handleClickListItem.bind(this);
   }
   //Insert Methods Here
+  handleClickListItem() {
+    this.setState({clicked: !this.state.clicked});
+  }
 
   render() {
 
     return (
-      <div className="panel list-item ">
+      <div className="panel list-item " onClick={this.handleClickListItem}>
         <span className="glyphicon glyphicon-globe col-md-1"></span> 
         <span className="col-md-4">{this.props.event.title}</span>
         <span className="col-md-4">{this.props.event.date} at {this.props.event.time}</span>
         <span className="col-md-3">{this.props.event.attendees}<span> people IN</span></span>
+        {this.state.clicked ? 'clicked' : '' }
         <br/>
       </div>
     );
