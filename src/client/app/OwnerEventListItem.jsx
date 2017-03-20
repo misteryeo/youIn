75294484a55fe8bd5efcd729a66a114fe7608ae0
David Brodie <david.brodie122@gmail.com>
2017-03-20 10:34:37 -0700

    Fix render bug that made fonts pass through panel line

--- src/client/app/OwnerEventListItem.jsx
+++ src/client/app/OwnerEventListItem.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const OwnerEventListItem = (props) => (
 
-  <div className="panel">
-    <span className="glyphicon glyphicon-globe list-item col-md-1"></span> 
-    <span className="list-item col-md-4">{props.event.title}</span>
-    <span className="list-item col-md-4">{props.event.date} at {props.event.time}</span>
-    <span className="list-item col-md-3">{props.event.attendees}<span> people IN</span></span>
+  <div className="panel list-item ">
+    <span className="glyphicon glyphicon-globe col-md-1"></span> 
+    <span className="col-md-4">{props.event.title}</span>
+    <span className="col-md-4">{props.event.date} at {props.event.time}</span>
+    <span className="col-md-3">{props.event.attendees}<span> people IN</span></span>
     <br/>
   </div>
 )
