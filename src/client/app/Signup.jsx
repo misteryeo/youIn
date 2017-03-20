50340b181ec435c0ed3ed4f2743d2d4ad03652e4
Augustus Brennan <augustusmb@gmail.com>
2017-03-20 16:18:48 -0700

    Add signup page that condtionally renders based on newUser state variable

---
+++ src/client/app/Signup.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+
+class Signup extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render () {
+
+    return (<div>
+      <h1 id="newUser">Yeah You're In!</h1>
+      <br></br><br></br><br></br>
+
+    </div>)
+  }
+}
+
+
+export default Signup;
