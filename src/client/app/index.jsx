8660f9e3971ef2a80c451f0edcc03735d4be7958
NicholasBelow <nkbelow@gmail.com>
2017-03-20 10:26:51 -0700

    Added Header class

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -54,15 +54,17 @@ class App extends React.Component {
         <CreateEventButton />
         <br /><br />
         <div className='container'>
-        <h2 className='col-md-4 col-md-offset-4'> MyEventsList</h2>
+          <h2 className='header-inner'> MyEventsList</h2>
+          <OwnerEventList  ownerEventsArr={this.state.ownerEvents}/>
         </div>
-        <OwnerEventList  ownerEventsArr={this.state.ownerEvents}/>
+        
 
         <br /><br />
         <div className='container'>
-        <h2 className='col-md-4 col-md-offset-4'> FriendEventList</h2>
+          <h2 className='header-inner'> FriendEventList</h2>
+          <FriendEventList friendEventsArr={this.state.friendEvents}/>
         </div>
-        <FriendEventList friendEventsArr={this.state.friendEvents}/>
+        
 
       </div>
     )
