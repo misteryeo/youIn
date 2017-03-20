b5efdc541fbc218f708a10436f9ac8215de2a4f7
David Brodie <david.brodie122@gmail.com>
2017-03-20 10:47:39 -0700

    Add styling/spacing to FriendEventListItem

--- src/client/app/FriendEventListItem.jsx
+++ src/client/app/FriendEventListItem.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const FriendEventListItem = (props) => (
 
-  <div className="well">
-    {props.event.title}
+  <div className="panel list-item ">
+    <span className="glyphicon glyphicon-globe col-md-1"></span> 
+    <span className="col-md-4">{props.event.title}</span>
+    <span className="col-md-4">{props.event.date} at {props.event.time}</span>
+    <span className="col-md-3">{props.event.attendees}<span> people IN</span></span>
+    <br/>
   </div>
 )
 
