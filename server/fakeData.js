27c7bae8d4edf7a71b84f1c218d2903ad83d9083
NicholasBelow <nkbelow@gmail.com>
2017-03-23 20:20:34 -0700

    Refactored fakeData to populate postgres tables

--- server/fakeData.js
+++ server/fakeData.js
@@ -9,29 +9,49 @@ var usersEvents = require('./data').usersEvents;
 
 // iterate over the users array
 users.forEach( (user) => {
-  db.queryAsync('INSERT into USERS SET ?', user)
+  db.query('INSERT into users VALUES (${user_id}, ${token}, ${firstname}, ${lastname}, ${photoUrl}, ${email})', user)
+ .then((result) => {
+   console.log('these are fake users', result);
+   // done(null, result);
+ })
+ .catch((err) => {
+   console.log('this is an error with users', err);
+   // done(err, null);
+ });
 });
 
 events.forEach( (event) => {
-  db.queryAsync('INSERT into EVENTS SET ?', event)
+  db.query('INSERT into EVENTS VALUES (${event_id}, ${owner}, ${title}, ${short_desc}, ${description}, ${location}, ${date}, ${time}, ${min})', event)
+  .then((result) => {
+    console.log('these are fake events', result);
+  })
+  .catch((err) => {
+    console.log('this is an error with events', err);
+  });
 });
 
 usersEvents.forEach( (userEvent) => {
-  db.queryAsync('INSERT into users_events SET ?', userEvent)
+  db.query('INSERT into users_events VALUES (${event_id}, ${user_id}, ${current_status})', userEvent)
+  .then((result) => {
+    console.log('these are fakes user_events', result);
+  })
+  .catch((err) => {
+    console.log('this is an error with the user_events', err);
+  });
 }); 
 
 
-db.queryAsync('SELECT * FROM users')
-.then( (result) => {
-  let users = result[0];
-  users.forEach((user1) => {
-    users.forEach((user2) => {
-      if (user1.user_id !== user2.user_id){
-        db.queryAsync('INSERT into friends SET ?', {user1: user1.user_id, user2: user2.user_id})
-      }
-    });
-  });
-});
+// db.query('SELECT * FROM users')
+// .then( (result) => {
+//   let users = result[0];
+//   users.forEach((user1) => {
+//     users.forEach((user2) => {
+//       if (user1.user_id !== user2.user_id){
+//         db.query('INSERT into friends SET ?', {user1: user1.user_id, user2: user2.user_id})
+//       }
+//     });
+//   });
+// });
   // add each to users table
     // add all events to events table
       // add all attendees to userEvents table
