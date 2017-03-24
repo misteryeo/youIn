2f1c553e58f8199fe57f804e293adcf12cfc0d61
David Brodie <david.brodie122@gmail.com>
2017-03-23 17:33:44 -0700

    Make get users work - error present but still get data (why?)

--- server/routes/get_users.js
+++ server/routes/get_users.js
@@ -4,5 +4,13 @@ let db = require('../config');
 
 module.exports = function(req, res) {
   console.log('inside get_users');
-  res.send('Suuuper inside it.');
+  db.query('select * from users')
+  .then ( (users) => {
+    console.log('result from query of users', users)
+    res.status(200).json(users);
+  })
+  .error( (err) => {
+    res.status(404).send(err, 'Error in get_users handler function');
+  })
+
 }
