e5f72d3826595e837e77da9385b951fb3b524031
Anthony Bianco <anthony.bianco3@gmail.com>
2017-03-24 22:33:46 -0700

    Correct query to the events table to get myEvents and friendEvents

--- server/models/get_events.js
+++ server/models/get_events.js
@@ -1,7 +1,18 @@
 let db = require('../config');
 
 module.exports = (id) => {
-  let query = 'SELECT * FROM EVENTS where owner=$1';
+  let query = 'SELECT events.event_id,\
+  events.owner,\
+  events.title,\
+  events.short_desc,\
+  events.description,\
+  events.location,\
+  events.date,\
+  events.time,\
+  events.min\
+  FROM EVENTS INNER JOIN users_events \
+  ON users_events.user_id=$1 AND events.event_id = users_events.event_id';
+
   return db.task( (t) => {
     return t.map(query, [2], (event) => {
       console.log(event);
@@ -9,7 +20,7 @@ module.exports = (id) => {
       INNER JOIN users_events ON users_events.current_status = \'accepted\' \
       AND users_events.event_id=$1 AND users.user_id=users_events.user_id';
       console.log(event.event_id);
-      return t.many(query, event.event_id)
+      return t.manyOrNone(query, event.event_id)
       .then( (attendees) => {
         event.attendees = attendees;
         return event;
