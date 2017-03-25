96afddcbc31d8a135dcdd703c5c110e71cf43f88
Anthony Bianco <anthony.bianco3@gmail.com>
2017-03-25 15:13:42 -0700

    return events in an object with 'ownerEvents' and 'friendEvents' properties

--- server/models/get_events.js
+++ server/models/get_events.js
@@ -14,7 +14,7 @@ module.exports = (id) => {
   ON users_events.user_id=$1 AND events.event_id = users_events.event_id';
 
   return db.task( (t) => {
-    return t.map(query, [id], (event) => {
+    return t.map(query, [2], (event) => {
       
       let query = 'SELECT users.user_id, users.firstname, users.lastname, users.photoUrl FROM users \
       INNER JOIN users_events ON users_events.current_status = \'accepted\' \
@@ -24,14 +24,21 @@ module.exports = (id) => {
         .then( (attendees) => {
           event.attendees = attendees;
           return event;
-        })
-        .catch( (err) => {
-          console.log('error in users_events table query', err);
         });
+        // .catch( (err) => {
+        //   console.log('error in users_events table query', err);
+        // });
     })
-    .catch( (err) => {
-      console.log('error in events table query', err);
-    })
-    .then(t.batch);
+    .then(t.batch)
+    .then( (results) => {
+      let events = { ownerEvents: [], friendEvents: []};
+      results.forEach( (event) => {
+        events[event.owner === id ? 'ownerEvents' : 'friendEvents'].push(event);
+      });
+      return events;
+    });
+    // .catch( (err) => {
+    //   console.log('error in events table query', err);
+    // });
   })
 }
