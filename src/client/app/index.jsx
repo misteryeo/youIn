187ecb825ac20c40dd764a483dd0ee945cf2f75a
David Brodie <david.brodie122@gmail.com>
2017-03-27 10:38:15 -0700

    Delete hardcoded Data for owner/friend events from Index.jsx

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -19,43 +19,8 @@ class App extends React.Component {
       friends: [],
       facebookToken: '',
       userName: '',
-      ownerEvents: [
-        {
-          event_id: 1,
-          owner: 1,
-          title: 'I Wanna Go To The Beach',
-          'short_desc': 'Beach Party',
-          description: 'Let\'s go to Ocean Beach while the sun is out! BYOB and don\'t joke about sharks, I\'ll kick you out for real.',
-          location: 'Ocean Beach, SF, right off the N Judah.',
-          date: '2017-03-14',
-          time: '05:30:00',
-          attendees: 10
-        }
-      ],
-      friendEvents: [
-        {
-          event_id: 2,
-          owner: 2,
-          title: 'Pig, Beer, and Bocce Ball',
-          'short_desc': 'BBQ',
-          description: 'Vegan Only; don\'t be a jerk about it.',
-          location: '456 Fillmore Street, San Francisco, CA 94117',
-          date: '2017-03-14',
-          time: '17:45:00',
-          attendees: 3
-        },
-        {
-          event_id: 3,
-          owner: 5,
-          title: 'Moonlight at 8',
-          'short_desc': 'Movie',
-          description: 'Let\'s go see Moonlight',
-          location: 'Downtown AMC Metreon',
-          date: '2017-03-14',
-          time: '20:00:00',
-          attendees: 6
-        }
-      ]
+      ownerEvents: [],
+      friendEvents: []
     }
 
     this.getUsers = this.getUsers.bind(this);
