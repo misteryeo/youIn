7564141c6a93c9ebe794a68039b1cfb433600417
Andy Yeo <andy.yeo@gmail.com>
2017-03-30 12:01:40 -0700

    Merge conflict

--- src/client/app/InviteFriendButton.jsx
+++ src/client/app/InviteFriendButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
+import InvitedFriends from './InvitedFriends.jsx';
+import InvitedFriendsEntry from './InvitedFriendsEntry.jsx'
 import Modal from 'boron/DropModal';
 import $ from 'jquery';
 
@@ -10,12 +12,20 @@ class InviteFriendButton extends React.Component {
       value: '',
       name: '',
       email: ''
+      // input: []
     }
     this.inviteUsers = this.inviteUsers.bind(this);
+    this.getInvitees = this.getInvitees.bind(this);
+    // this.addInput = this.addInput.bind(this);
+  }
+
+  componentDidMount() {
+    this.getInvitees();
   }
 
   showModal () {
     this.refs.modal.show();
+    this.getInvitees();
   }
 
   hideModal () {
@@ -42,13 +52,41 @@ class InviteFriendButton extends React.Component {
       data: JSON.stringify(newUser),
       success: function(data) {
         console.log('Invite success', data)
-        context.hideModal();
+        context.getInvitees();
       },
       error: function(error) {
         console.error('Error', error)
       }
     })
   }
+
+  getInvitees(event) {
+    let context = this;
+    $.ajax({
+      url: '/invitees',
+      method: 'GET',
+      success: function(data) {
+        console.log('Successful invitee retrieval', data)
+          this.setState({
+            invites: data
+          })
+      }.bind(this),
+      error: function(error) {
+        console.error('Error', error)
+      }
+    })
+  }
+
+  // Testing out ability to add extra friend and name input fields
+  // addInput(event) {
+  //   const input = this.refs.input
+  //   const addInput = this.state.input
+  //   addInput.push(input)
+  //   console.log(addInput)
+  //   this.setState({
+  //     input: addInput
+  //   })
+  // }
 
   render() {
     return(
@@ -59,10 +97,24 @@ class InviteFriendButton extends React.Component {
           <h2>Be awesome - invite your friends</h2>
             <div className="row">
               <div className="col-md-8">
-                <h4 className='create'>You know you want to:</h4>
-                Name: <input placeholder="Name" value={this.state.name} onChange={this.handleChange.bind(this, 'name')}/><br />
-                Email: <input placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this, 'email')}/><br />
+                <h4 className='create'>Friends who will love you:</h4>
+                <InvitedFriends data={this.state.invites}/>
                 <br />
+              </div>
+            </div>
+
+
+            <div className="row">
+              <div className="col-md-8">
+                <h4 className='create'>You know you want to:</h4>
+                <div ref="input">
+                  Name: <input placeholder="Name" onChange={this.handleChange.bind(this, 'name')}/><br />
+                  Email: <input placeholder="Email" onChange={this.handleChange.bind(this, 'email')}/><br />
+                  <br />
+                </div>
+              </div>
+              <div className="col-md-12">
+                <button type="submit" onClick={this.addInput}>+</button>
               </div>
               <div className="col-md-12">
                 <button type="submit" onClick={this.inviteUsers}>Invite!</button>
