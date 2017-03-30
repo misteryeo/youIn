faad9bb5aa71e43a542b79eaa4439b269ea785c4
Andy Yeo <andy.yeo@gmail.com>
2017-03-30 11:59:48 -0700

    Merge conflict

---
+++ server/routes/invite_users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let db = require('../config');
+
+module.exports = function(req, res) {
+  let name = req.body.name;
+  let email = req.body.email;
+  console.log("hello", req.body);
+
+
+  db.query('INSERT into INVITES (name, email) VALUES ($1, $2)', [name, email])
+  .then((result) => {
+    res.status(201).send('');
+  })
+
+}
