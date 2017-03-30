443dfb385a46ce18475802f556731b9bacd3097e
Andy Yeo <andy.yeo@gmail.com>
2017-03-30 11:51:08 -0700

    Update appID details

--- src/client/app/Facebook.jsx
+++ src/client/app/Facebook.jsx
@@ -9,8 +9,9 @@ class Facebook extends React.Component {
   componentDidMount() {
     window.fbAsyncInit = function() {
       FB.init({
-        appId      : window.location.host === 'localhost:8080' ? '1897409527137678' : window.location.host === 'you-in.herokuapp.com' ? '1724251701198619' :'414888392204388',
-        cookie     : true,
+        appId      : window.location.host === 'localhost:8080' ? '1678389805794701' : window.location.host === 'you-in.herokuapp.com' ? '1724251701198619' :'414888392204388',
+        cookie     : true,  // enable cookies to allow the server to access
+                          // the session 1286128124802062
         xfbml      : true,
         version    : 'v2.1'
       });
