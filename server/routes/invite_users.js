d948e07abe3ccdb5e1cd7fbdac3edb45dc57d31e
Andy Yeo <andy.yeo@gmail.com>
2017-04-01 18:25:33 -0700

    Merge conflicts

--- server/routes/invite_users.js
+++ server/routes/invite_users.js
@@ -5,8 +5,6 @@ let db = require('../config');
 module.exports = function(req, res) {
   let name = req.body.name;
   let email = req.body.email;
-  console.log('UserID', req.user.user_id);
-
 
   db.query('INSERT into INVITES (name, email) VALUES ($1, $2)', [name, email])
   .then((result) => {
